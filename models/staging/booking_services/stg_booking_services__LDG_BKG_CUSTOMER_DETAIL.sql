192b91e74e0a182fe53f59ac2ae53181ea8a8382
Scott Abbott <sabbott@doc.govt.nz>
2024-06-10 02:03:48 +0000

    PK tests working

---
+++ models/staging/booking_services/stg_booking_services__LDG_BKG_CUSTOMER_DETAIL.sql
@@ -0,0 +1,38 @@
+with 
+
+source as (
+
+    select * from {{ source('booking_services', 'LDG_BKG_CUSTOMER_DETAIL') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+        account_number,
+        customer_first_name,
+        customer_last_name,
+        customer_country,
+        customer_state,
+        customer_zip,
+        customer_email,
+        customer_primary_phone,
+        gender,
+        account_creation_date,
+        receive_newsletter,
+        participant_survey,
+        dob,
+        classification_expiry_date,
+        organization,
+        user_type,
+        last_modify_date,
+        environment,
+        customer_classification,
+        datetime_updated
+
+    from source
+
+)
+
+select * from renamed
