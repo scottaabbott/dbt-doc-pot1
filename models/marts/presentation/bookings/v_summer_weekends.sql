0c9f11c099a16ecc03b1af0ab77739d18cb310ce
Scott Abbott <sabbott@doc.govt.nz>
2024-06-13 00:11:01 +0000

    dim_date wkg - v_summer_weekends - not wkg

---
+++ models/marts/presentation/bookings/v_summer_weekends.sql
@@ -0,0 +1,21 @@
+with 
+    source as 
+    (
+        select * from {{ ref('dim_dates') }}
+    ),
+
+    renamed as 
+    (
+        select                     
+                    date as v_date,
+                    day_of_week_name
+        from
+                    source
+        where
+                    date between '2024-06-20' -- a Thursday
+                            and '2024-09-22'  -- a Sunday
+            and     is_weekend
+    )
+
+select * from renamed
+
